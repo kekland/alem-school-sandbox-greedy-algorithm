36c4692ea331a78ebf53b4b5adb2473543cb02ce
kekland <kk.erzhan@gmail.com>
2021-11-16 00:59:49 +0600

    impl: replay player

--- visualizer/src/components/Block.js
+++ visualizer/src/components/Block.js
@@ -7,11 +7,13 @@ import Dagger from '../assets/dagger.png'
 import Monster from '../assets/monster.png'
 import MonsterRealm from '../assets/monster-realm.png'
 import Player from '../assets/player.png'
+import PlayerOther from '../assets/player-2.png'
 import { lerpColor } from '../utils'
-
 
 export const BlockComponent = ({ block, safety, visibility, entities, monsterRealm, onChange, poi }) => {
   let contentImgs = [];
+  let text1 = [];
+  let text2 = [];
 
   if (block === Block.bonus) {
     contentImgs.push(Bonus);
@@ -23,8 +25,35 @@ export const BlockComponent = ({ block, safety, visibility, entities, monsterRea
     contentImgs.push(Dagger);
   }
 
-  if (entities.find((v) => v.type === 'player')) {
+  const player = entities.find((v) => v.type === 'player')
+  if (player) {
     contentImgs.push(Player);
+    if (player.name) {
+      text1.push(...player.name.split(','))
+    }
+
+    if (player.dagger) {
+      text1.push('Dagger')
+    }
+    if (player.bonus) {
+      text1.push('Bonus')
+    }
+  }
+
+  const otherPlayer = entities.find((v) => v.type === 'player-other')
+  if (otherPlayer) {
+    contentImgs.push(PlayerOther);
+
+    if (otherPlayer.name) {
+      text2.push(...otherPlayer.name.split(','));
+    }
+
+    if (otherPlayer.dagger) {
+      text2.push('Dagger')
+    }
+    if (otherPlayer.bonus) {
+      text2.push('Bonus')
+    }
   }
 
   if (entities.find((v) => v.type === 'monster')) {
@@ -54,6 +83,11 @@ export const BlockComponent = ({ block, safety, visibility, entities, monsterRea
       {
         contentImgs.map((v) => <img key={v} dswidth={42} height={42} src={v} alt='lol' />)
       }
+      <span style={{ position: 'absolute', fontSize: 10, opacity: 0.75, width: 100, paddingTop: 48, zIndex: 5, textAlign: 'center' }}>
+        <span style={{ color: '#619bfa' }}>{text1 ? text1.join(' ') : ''}</span>
+        {text1.length > 0 && text2.length > 0 ? <><br /> <br /></> : null}
+        <span style={{ color: '#60a75b' }}>{text2 ? text2.join(' ') : ''}</span>
+      </span>
     </div>
   )
 }
---
+++ visualizer/src/components/ReplayPanel.js
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react'
+import { getReplayData } from '../replay-parser'
+
+export const ReplayPanel = ({ replayData, replayFrame, scores, setReplayData, setReplayFrame }) => {
+  const inputRef = useRef()
+
+  const isReplayFrameValid = (n) => {
+    if (n < 0) return false;
+    if (n > replayData.frames.length) return false;
+    return true;
+  }
+
+  const diffReplayFrame = (i) => {
+    const newFrames = replayFrame + i;
+    if (isReplayFrameValid(newFrames)) {
+      setReplayFrame(newFrames);
+    }
+  }
+
+
+  return (
+    <div>
+      <span className='caption'>Game url or id</span> <br />
+      <input ref={inputRef} style={{ width: 400 }} /> <span>&nbsp;&nbsp;</span>
+      <button onClick={async () => {
+        const data = await getReplayData(inputRef.current.value)
+
+        setReplayFrame(0);
+        setReplayData(data);
+      }}>Load</button>
+      {
+        replayData ? <div style={{ marginTop: 12 }}>
+          <button onClick={() => diffReplayFrame(-1)}>-1</button>
+          <span>&nbsp;&nbsp;</span>
+          <input value={replayFrame} onChange={(v) => {
+            try {
+              const value = parseInt(v.target.value.length === 0 ? '0' : v.target.value);
+              if (!isNaN(value) && isReplayFrameValid(value)) {
+                setReplayFrame(value)
+              }
+            }
+            catch (e) { }
+          }} style={{ width: 60 }} />
+          <span>&nbsp;&nbsp;</span>
+          <button onClick={() => diffReplayFrame(1)}>+1</button>
+          <span className='caption'>
+            &nbsp; &nbsp; Frames: {replayData.frames.length} &nbsp; &nbsp;
+          </span>
+          <button onClick={() => {
+            setReplayData(null);
+            setReplayFrame(null);
+          }}>Clear</button> <br /> <br />
+          <button onClick={() => {
+            setReplayData({
+              ...replayData,
+              playerTeamId: replayData.result
+                .find((v) => v.team_id !== replayData.playerTeamId)?.team_id ?? replayData.playerTeamId
+            })
+          }}>Swap targeted player</button> <br />
+          <span className='caption'>
+            {replayData.result.map((v) => {
+              if (v.team_id === replayData.playerTeamId) {
+                return <span style={{ color: '#619bfa' }}>{v.username}: {scores[v.team_id]} &nbsp; </span>
+              }
+
+              return <span style={{ color: '#60a75b' }}>{v.username}: {scores[v.team_id]} &nbsp;</span>
+            })}
+          </span>
+        </div> : null
+      }
+    </div >
+  )
+}
